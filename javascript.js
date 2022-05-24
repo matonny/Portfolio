ae4d9c59829345746231ace02ffa8f9f37f6dba9
matonny <onny.mat@gmail.com>
2022-05-24 16:33:08 +0100

    minor updates

--- javascript.js
+++ javascript.js
@@ -1,36 +1,43 @@
-$('a[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-        var target = $(this.hash);
-        var id = this.hash.slice(1);
-        target = target.length ? target : $('[name=' + id + ']');
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000, function () {
-                location.hash = id;
-            });
-            return false;
+// $('a[href*="#"]:not([href="#"])').click(function () {
+//     if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+//         var target = $(this.hash);
+//         var id = this.hash.slice(1);
+//         target = target.length ? target : $('[name=' + id + ']');
+//         if (target.length) {
+//             $('html, body').animate({
+//                 scrollTop: target.offset().top
+//             }, 1000, function () {
+//                 location.hash = id;
+//             });
+//             return false;
+//         }
+//     }
+// });
+
+(function(){
+
+    const openMenuButton = document.querySelector("#open-menu-button");
+    const navigationLinks = document.querySelectorAll(".header__link")
+    
+    setup();
+
+    function setup(){
+        openMenuButton.addEventListener("click", manageMenu);
+        navigationLinks.forEach((link)=>{
+            link.addEventListener("click", closeMenu);
+        });
+    }
+    function closeMenu() {
+        openMenuButton.setAttribute('aria-expanded', 'false');
+    }
+    function openMenu() {
+        openMenuButton.setAttribute('aria-expanded', 'true');
+    }
+    function manageMenu(){
+        if(openMenuButton.getAttribute('aria-expanded') === 'true'){
+            closeMenu();
+        }else{
+            openMenu();
         }
     }
-});
-
-var $openMenuButton = $('.js-open-menu');
-var $navigationLinks = $('.header__nav a');
-
-function openMenu() {
-    $openMenuButton.attr('aria-expanded', 'true');
-}
-
-function closeMenu() {
-    $openMenuButton.attr('aria-expanded', 'false');
-}
-
-$openMenuButton.click(function () {
-    if ($openMenuButton.attr('aria-expanded') === 'true') {
-        closeMenu();
-    } else {
-        openMenu();
-    }
-});
-
-$navigationLinks.click(closeMenu);
+}());
